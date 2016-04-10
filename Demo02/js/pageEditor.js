2345dff6cac452d712c22c2a7d9808f1afa856eb
Barbara <barbara-sikora@gmx.at>
2016-04-10 10:54:59 +0200

    Update

--- Demo02/js/pageEditor.js
+++ Demo02/js/pageEditor.js
@@ -1,67 +1,97 @@
 $(function () {
     var options = {
-        float:false,
+        float:true,
         staticGrid:true,
         removable: '.trash',
         removeTimeout: 100,
         acceptWidgets: '.grid-stack-item'
     };
-    $('.grid-stack').gridstack(options);
+    var gridStack =   $('.grid-stack');
+    var makeEditable;
+    gridStack.gridstack(options);
+
 
     new function () {
         this.images = [
             {x: 3, y: 1, width: 6, height: 6}
         ];
         this.texts = [
-            {x: 3, y: 0, width: 6, height: 1},
-            {x: 3, y: 7, width: 6, height: 2}
+            {x: 3, y: 0, width: 6, height: 1, content:"Enter your text"},
+            {x: 3, y: 7, width: 6, height: 2, content:"Enter your text"}
         ];
         this.links = [
-            {x: 2, y: 9, width: 4, height: 1},
-            {x: 6, y: 9, width: 4, height: 1},
-            {x: 2, y: 10, width: 4, height: 1},
-            {x: 6, y: 10, width: 4, height: 1}
-        ];
-        this.items = [
-            {x: 0, y: 0, width: 2, height: 2},
-            {x: 3, y: 1, width: 1, height: 2},
-            {x: 4, y: 1, width: 1, height: 1},
-            {x: 2, y: 3, width: 3, height: 1},
-            {x: 2, y: 5, width: 1, height: 1}
+            {x: 2, y: 9, width: 4, height: 1, content:"Enter your link-text"},
+            {x: 6, y: 9, width: 4, height: 1, content:"Enter your link-text"},
+            {x: 2, y: 10, width: 4, height: 1, content:"Enter your link-text"},
+            {x: 6, y: 10, width: 4, height: 1, content:"Enter your link-text"}
         ];
 
         var grid = $('#inner').data('gridstack');
+        var editButton = $('#edit');
 
+        this.newTextWidget = function(){
+            var el = '<div class="text grid-stack-item"><button class="delete hidden">X</button><div class="grid-stack-item-content">ADD TEXT</div></div>';
+            $('#textWidget').append(el);
+            $('#textWidget .text').draggable({
+                revert: 'invalid',
+                handle: '.grid-stack-item-content',
+                scroll: false,
+                appendTo: '#inner'
+            });
+            $('#textWidget .text').on('remove',this.newTextWidget);
+        }.bind(this);
 
-        this.edit = function(){
+        this.newLinkWidget = function(){
+            var el = '<div class="link grid-stack-item"><button class="delete hidden">X</button><div class="grid-stack-item-content"><div><a href="#">ADD LINK</a></div></div></div>';
+            $('#linkWidget').append(el);
+            $('#linkWidget .link').draggable({
+                revert: 'invalid',
+                handle: '.grid-stack-item-content',
+                scroll: false,
+                appendTo: '#inner'
+            });
+            $('#linkWidget .link').on('remove',this.newLinkWidget);
+        }.bind(this);
 
-            if($('#edit').text() == 'Edit'){
-                $('.grid-stack-item-content').addClass('grid-border');
-                $('.delete').on('click',this.deleteWidget);
-                $('.delete').removeClass('hidden');
-                $('.text .grid-stack-item-content').each(function() {
+         makeEditable = function(){
+             $('.grid-stack .delete').on('click',this.deleteWidget);
+            $('.grid-stack .delete').each(function(){
+                if($('.grid-stack .delete').hasClass('hidden')){
+
+                    $(this).removeClass('hidden');
+                }
+            });
+            $('.grid-stack .text .grid-stack-item-content').each(function() {
+                if($(this).find('textarea').length == 0){
                     var t = $(this).text();
                     $(this).text('');
                     $(this).append('<textarea>'+t+'</textarea>');
-                });
-                $('.link .grid-stack-item-content div:first-of-type').each(function() {
+                }
+            });
+            $('.grid-stack .link .grid-stack-item-content div:first-of-type').each(function() {
+                if($(this).find('input').length == 0){
                     var l = $(this).text();
                     $(this).find('a').addClass('hidden');
                     $(this).append('<input type="text" value="'+l+'">');
-                });
+                }
+            });
+        }.bind(this);
 
-                    $('#inner').data('gridstack').setStatic(false);
 
-                $('#edit').text('Save');
-            }else if($('#edit').text() == 'Save'){
-                $('.grid-stack-item-content').removeClass('grid-border');
-                $('.delete').addClass('hidden');
-                $('.text .grid-stack-item-content textarea').each(function() {
+        this.edit = function(){
+            $('.sidebar').slideToggle('fast');
+            if(editButton.text() == 'Edit'){
+                makeEditable();
+                $('#inner').data('gridstack').setStatic(false);
+                editButton.text('Save');
+            }else if(editButton.text() == 'Save'){
+                $('.grid-stack .delete').addClass('hidden');
+                $('.grid-stack .text .grid-stack-item-content textarea').each(function() {
                     var t = $(this).val();
                     $(this).parent().text(t);
                     $(this).remove();
                 });
-                $('.link .grid-stack-item-content input').each(function() {
+                $('.grid-stack .link .grid-stack-item-content input').each(function() {
                     var l = $(this).val();
                     $(this).parent().find('a').text(l);
                     $(this).parent().find('a').removeClass('hidden');
@@ -71,22 +101,11 @@ $(function () {
 
                     $('#inner').data('gridstack').setStatic(true);
 
-                $('#edit').text('Edit');
+                editButton.text('Edit');
             }
             return false;
         }.bind(this);
 
-        this.addNewWidget = function () {
-            var node = this.items.pop() || {
-                    x: 12 * Math.random(),
-                    y: 5 * Math.random(),
-                    width: 1 + 3 * Math.random(),
-                    height: 1 + 3 * Math.random()
-                };
-            grid.addWidget($('<div><div class="grid-stack-item-content" /><div/>'),
-                node.x, node.y, node.width, node.height);
-            return false;
-        }.bind(this);
 
         this.deleteWidget = function (e) {
             grid.remove_widget(e.currentTarget.offsetParent);
@@ -114,12 +133,7 @@ $(function () {
         this.loadText = function () {
             var texts = GridStackUI.Utils.sort(this.texts);
             _.each(texts, function (node) {
-                grid.addWidget($('<div class="text"><button class="delete hidden">X</button><div class="grid-stack-item-content">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy ' +
-                        'eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea ' +
-                        'rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' +
-                        'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam' +
-                        ' et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ' +
-                        'Lorem ipsum dolor sit amet.<div/><div/>'),
+                grid.addWidget($('<div class="text"><button class="delete hidden">X</button><div class="grid-stack-item-content">'+node.content+'<div/><div/>'),
                     node.x, node.y, node.width, node.height);
             }, this);
             return false;
@@ -128,7 +142,7 @@ $(function () {
         this.loadLinks = function () {
             var links = GridStackUI.Utils.sort(this.links);
             _.each(links, function (node) {
-                grid.addWidget($('<div class="link"><button class="delete hidden">X</button><div class="grid-stack-item-content"><div><a href="#">LINK</a></div><div/><div/>'),
+                grid.addWidget($('<div class="link"><button class="delete hidden">X</button><div class="grid-stack-item-content"><div><a href="#">'+node.content+'</a></div><div/><div/>'),
                     node.x, node.y, node.width, node.height);
             }, this);
             return false;
@@ -166,10 +180,11 @@ $(function () {
                     x: node.x,
                     y: node.y,
                     width: node.width,
-                    height: node.height
+                    height: node.height,
+                    content: el.find('.grid-stack-item-content').text()
                 };
             }, this);
-            $('#saved-data').val(JSON.stringify(this.texts, null, '    '));
+            $('#saved-data').val($('#saved-data').val()+JSON.stringify(this.texts, null, '    '));
             return false;
         }.bind(this);
 
@@ -181,10 +196,11 @@ $(function () {
                     x: node.x,
                     y: node.y,
                     width: node.width,
-                    height: node.height
+                    height: node.height,
+                    content: el.find('a').text()
                 };
             }, this);
-            $('#saved-data').val(JSON.stringify(this.links, null, '    '));
+            $('#saved-data').val( $('#saved-data').val()+JSON.stringify(this.links, null, '    '));
             return false;
         }.bind(this);
 
@@ -194,20 +210,33 @@ $(function () {
             return false;
         }.bind(this);
 
+
         $('#save-grid').click(this.saveGrid);
         $('#load-grid').click(this.loadGrid);
         $('#clear-grid').click(this.clearGrid);
-        $('#edit').click(this.edit);
-        $('#add-new-widget').click(this.addNewWidget);
+        editButton.click(this.edit);
+        $('#textWidget .text').on('remove',this.newTextWidget);
+        $('#linkWidget .link').on('remove',this.newLinkWidget);
 
-        this.loadGrid();
 
-        $('.sidebar .grid-stack-item').draggable({
-            revert: 'invalid',
-            handle: '.grid-stack-item-content',
-            scroll: false,
-            appendTo: '#inner'
-        });
+            this.loadGrid();
+
+            $('.sidebar .grid-stack-item').draggable({
+                revert: 'invalid',
+                handle: '.grid-stack-item-content',
+                scroll: false,
+                appendTo: '#inner'
+            });
+
 
     };
+
+    //ausschaltn wenns nur geladen wird
+
+    gridStack.on('change',function(){
+        if($('#edit').text() == 'Save'){
+            makeEditable();
+        }
+
+    });
 });
