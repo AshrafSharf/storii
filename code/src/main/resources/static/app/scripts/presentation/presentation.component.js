563200aee13220fcf073d1835c0fdf0c1da7e34a
David Hondl <david.hondl@gmx.net>
2016-06-27 12:32:19 +0200

    fullscreen mode way more beautiful

--- code/src/main/resources/static/app/scripts/presentation/presentation.component.js
+++ code/src/main/resources/static/app/scripts/presentation/presentation.component.js
@@ -127,9 +127,19 @@ System.register(['angular2/core', 'angular2/router', '../logState/logState.compo
                 };
                 PresentationComponent.prototype.toggleFullScreen = function () {
                     var target = jQuery('.fullScreenBox')[0];
+                    var inner = jQuery('#outer');
+                    var buttonF = jQuery('.fullScreenToggle')[0];
                     if (screenfull.enabled) {
+                        if (!screenfull.isFullscreen) {
+                            target.style.width = '100%';
+                            inner.css('border', 'none');
+                            buttonF.src = 'app/assets/files/dartdown.png';
+                        }
+                        else {
+                            inner.css('border', '2px dotted black');
+                            buttonF.src = 'app/assets/files/dartup.png';
+                        }
                         screenfull.toggle(target);
-                        target.style.width = '100%';
                     }
                 };
                 PresentationComponent.prototype.goToRate = function () {
--- code/src/main/resources/static/app/scripts/presentation/presentation.component.ts
+++ code/src/main/resources/static/app/scripts/presentation/presentation.component.ts
@@ -134,9 +134,18 @@ export class PresentationComponent implements OnInit {
    
    toggleFullScreen(){
    		var target = jQuery('.fullScreenBox')[0];
+   		var inner =  jQuery('#outer');
+   		var buttonF = jQuery('.fullScreenToggle')[0];
 		if (screenfull.enabled) {
-		    screenfull.toggle(target);
-		    target.style.width = '100%';
+			if(!screenfull.isFullscreen{
+			    target.style.width = '100%';
+			    inner.css('border', 'none');
+			    buttonF.src= 'app/assets/files/dartdown.png';
+			}else{
+				inner.css('border', '2px dotted black');
+				buttonF.src= 'app/assets/files/dartup.png';
+			}
+			screenfull.toggle(target);
 		}
 
    }
